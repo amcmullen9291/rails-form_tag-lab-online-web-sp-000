583385c75780b356ff30653aa05ea2666b344609
anthony mcmullen <amcmullen9291@gmail.com>
2021-01-27 01:40:19 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,14 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+    binding.pry
   end
-
+  
+  def new 
+  end
+  
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to student_path(@student)
+  end
 end
